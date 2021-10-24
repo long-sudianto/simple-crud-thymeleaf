489926aea990b2cda59a7368dfecfda9811a4c01
Sudianto Long <long.sudianto@gmail.com>
2021-10-24 19:19:33 +0700

    updated

--- src/main/java/com/example/simplecrud/controller/GreetingController.java
+++ src/main/java/com/example/simplecrud/controller/GreetingController.java
@@ -2,11 +2,15 @@ package com.example.simplecrud.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.simplecrud.model.Tutorial;
+import com.example.simplecrud.service.TutorialService;
 
 @Controller
 public class GreetingController {
@@ -16,6 +20,8 @@ public class GreetingController {
 		model.addAttribute("name", name);
 		return "greeting";//greeting.html
 	}
+	
+
 	
 	
 	
@@ -34,5 +40,47 @@ public class GreetingController {
 		String dateStr = dateFormat.format(now);
 		model.addAttribute("time", dateStr);
 		return "index";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@GetMapping("/rest")
+	public String getTutorials() {
+		TutorialService service = new TutorialService();
+		service.printTutorial();
+		return "index";
+	}
+	
+	
+	
+	
+	@GetMapping("/rest2")
+	public String getTutorials2() {
+		TutorialService service = new TutorialService();
+		Tutorial tutorial = service.getTutorialsById();
+    	System.out.println("Title : " + tutorial.getTitle());
+    	System.out.println("Description : " + tutorial.getDescription());
+		return "index";
+	}
+	
+	
+	
+	
+	@GetMapping("/rest3")
+	public String getTutorials3(Model model) {
+		TutorialService service = new TutorialService();
+		List<Tutorial> tutorials = service.getAllTutorials();
+		model.addAttribute("listTutorials", tutorials);
+		return "list";
 	}
 }
---
+++ src/main/java/com/example/simplecrud/service/TutorialService.java
@@ -0,0 +1,80 @@
+package com.example.simplecrud.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.simplecrud.model.Tutorial;
+
+@Service
+public class TutorialService {
+        
+	public void printTutorial(){
+    	RestTemplate restTemplate = new RestTemplate();
+    	
+    	String response = restTemplate.getForObject("http://localhost:8080/api/tutorials/1", String.class);
+    	System.out.println("http://localhost:8080/api/tutorials/1 : " + response.toString());
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public Tutorial getTutorialsById(){
+    	RestTemplate restTemplate = new RestTemplate();
+    	
+    	Tutorial response = restTemplate.getForObject("http://localhost:8080/api/tutorials/1", Tutorial.class);
+    	System.out.println("http://localhost:8080/api/tutorials/1 ke 2 : " + response.toString());    	
+    	return response;
+    }
+    
+    
+    
+
+    public List<Tutorial> getAllTutorials() {
+    	RestTemplate restTemplate = new RestTemplate();
+    	List<Tutorial> tutorials = new ArrayList<Tutorial>();
+    	
+    	//ResponseEntity<List<Tutorial>> response = restTemplate.exchange("http://localhost:8080/api/tutorials/", HttpMethod.GET, HttpEntity.EMPTY, new ParameterizedTypeReference<List<Tutorial>>(){});
+        ResponseEntity<Tutorial[]> response = restTemplate.getForEntity("http://localhost:8080/api/tutorials/", Tutorial[].class);
+                
+        for (int i = 0; i < response.getBody().length ; i++) {
+        	tutorials.add(response.getBody()[i]);
+		}        
+        
+    	return tutorials;
+    }
+    
+    
+    
+//    
+//    public User getUserById(Long id) {
+//        ResponseEntity<Response<User>> response = restTemplate.exchange(usersApiWithId, HttpMethod.GET, HttpEntity.EMPTY, new ParameterizedTypeReference<Response<User>>(){}, id);
+//        log.info("getUserById response: "+response.getBody().toString());
+//        User user = response.getBody().getPayload();
+//        return user;
+//    }
+//    
+//    public User saveUser(User user) {
+//        return restTemplate.postForObject(usersApi, user, User.class);
+//    }
+//
+//    public User update(Long id, User user) {
+//        return restTemplate.exchange(usersApiWithId, HttpMethod.PUT, new HttpEntity<>(user), User.class, id).getBody();
+//    }
+
+}
